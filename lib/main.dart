96e3192a31dd01624ff7f5d941fbb5e13b425520
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-05-29 14:27:28 +0530

    added bluetoothconnection state method

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,11 +26,58 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
+
+  FlutterBluetoothSerial bluetooth = FlutterBluetoothSerial.instance;
+
+  List<BluetoothDevice> _devicesList = [];
+  BluetoothDevice _device;
+  bool _connected = false;
+  bool _pressed = false;
+
+  @override
+  void initState(){
+    super.initState();
+    bluetoothConnectoinState();
+  }
+
+  Future<void> bluetoothConnectoinState() async {
+    List<BluetoothDevice> devices = [];
+
+    try {
+      devices = await bluetooth.getBondedDevices();
+    } catch (error) {
+      print(error);
+    }
+
+    bluetooth.onStateChanged().listen((event) {
+      switch (event) {
+        case FlutterBluetoothSerial.CONNECTED:
+          setState(() {
+            _connected = true;
+            _pressed = true;
+          });
+          break;
+
+        case FlutterBluetoothSerial.DISCONNECTED:
+          setState(() {
+            _connected = false;
+            _pressed = false;
+          });
+          break;
+
+        default:
+          print(event);
+          break;
+      }
+
+      setState(() {
+        _devicesList = devices;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
-
